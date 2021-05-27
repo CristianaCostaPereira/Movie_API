0d25702a2e84230a72b6522823d8764554fc3caf
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-05-27 10:55:48 +0100

    Creating table and pagination

--- movies-api/src/views/Todos.vue
+++ movies-api/src/views/Todos.vue
@@ -1,11 +1,62 @@
 <template>
-  <div class="about">
-    <h1>This is a Todos page</h1>
-
+  <div class="container">
+    <!-- Criar tarefa -->
     <div class="row">
-      <div class="col-lg-4">1</div>
-      <div class="col-lg-4">2</div>
-      <div class="col-lg-4">3</div>
+      <div class="col-lg-6">
+        <button
+          type="button"
+          class="btn btn-outline-primary">
+
+          Criar tarefa
+        </button>
+      </div>
+    </div>
+
+    <!-- Listagem de todos -->
+    <div class="row mt-4">
+      <div class="col-lg-12">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item"><a class="page-link" href="#">Página Anterior</a></li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">Página Seguinte</a></li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
