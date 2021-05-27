2a425096d652c1a0d3a51d065f75c63441f73c9d
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-05-27 10:47:57 +0100

    Bootstrap installed

--- movies-api/src/views/Todos.vue
+++ movies-api/src/views/Todos.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is a Todos page</h1>
+
+    <div class="row">
+      <div class="col-lg-4">1</div>
+      <div class="col-lg-4">2</div>
+      <div class="col-lg-4">3</div>
+    </div>
   </div>
 </template>
