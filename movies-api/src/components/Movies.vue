99a5e204a9b3fae7fd7b1c2a66b48431bb8b0ce0
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-05-29 12:51:48 +0100

    backgroun-image added

--- movies-api/src/components/Movies.vue
+++ movies-api/src/components/Movies.vue
@@ -23,7 +23,13 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+  body {
+    background-image: url('https://images.unsplash.com/photo-1562329265-95a6d7a83440?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=964&q=80');
+    background-repeat: no-repeat;
+    background-size: 100%;
+  }
+  
   .form-control::placeholder {
     color: #bebcbc;
   }
